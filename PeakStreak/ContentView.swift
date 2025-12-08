928d2dcb6aeea097b84b92eb3b4262a39782224b
Siddharth <singhsiddharth174@gmail.com>
2025-12-08 17:31:46 -0500

    Initial Commit

---
+++ PeakStreak/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PeakStreak
+//
+//  Created by Siddharth on 08/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PeakStreak/PeakStreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeakStreakApp.swift
+//  PeakStreak
+//
+//  Created by Siddharth on 08/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PeakStreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
